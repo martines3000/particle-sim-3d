38bba9b006baafd646cf523096f61fdbd9781fe2
martines3000 <domajnko.martin@gmail.com>
2022-12-12 18:50:28 +0100

    feat(RA): vaja_3

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
-use bevy::{prelude::*, window::PresentMode};
+use std::f32::consts::PI;
+
+use bevy::{
+    diagnostic::{Diagnostics, FrameTimeDiagnosticsPlugin},
+    prelude::*,
+    window::PresentMode,
+};
 mod particle;
 mod world;
 
-use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
-
-use bevy_inspector_egui::WorldInspectorPlugin;
-use particle::ParticlePlugin;
+use bevy_egui::{egui, EguiContext, EguiPlugin};
+use particle::{Lifetime, ParticlePlugin};
 use world::WorldPlugin;
+
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::rgb(0.2, 0.2, 0.2)))
+        .insert_resource(Msaa { samples: 1 })
+        .init_resource::<GlobalState>()
         .add_startup_system(spawn_camera)
         .add_startup_system(setup_scene)
+        .add_startup_system(configure_global_state)
         .add_system(camera_controls)
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             window: WindowDescriptor {
@@ -23,31 +31,114 @@ fn main() {
             },
             ..default()
         }))
-        .add_plugin(FrameTimeDiagnosticsPlugin::default())
-        .add_plugin(LogDiagnosticsPlugin::default())
-        .add_plugin(WorldInspectorPlugin::new())
         .add_plugin(WorldPlugin)
         .add_plugin(ParticlePlugin)
+        .add_plugin(EguiPlugin)
+        .add_plugin(FrameTimeDiagnosticsPlugin::default())
+        .add_system(fps_update_system)
+        .add_system(particle_count_update_system)
+        .add_system(ui)
         .run();
 }
 
 fn spawn_camera(mut commands: Commands) {
     commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(10.0, 0.0, 0.0).looking_at(Vec3::ZERO, Vec3::Y),
+        transform: Transform::from_xyz(25.0, 0.0, 25.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
 }
 
-fn setup_scene(mut commands: Commands) {
-    commands.spawn(PointLightBundle {
-        point_light: PointLight {
-            intensity: 1000.0,
-            shadows_enabled: false,
+fn setup_scene(mut commands: Commands, asset_server: Res<AssetServer>) {
+    // DIRECTIONAL LIGHT
+    const HALF_SIZE: f32 = 10.0;
+    commands.spawn(DirectionalLightBundle {
+        directional_light: DirectionalLight {
+            // Configure the projection to better fit the scene
+            shadow_projection: OrthographicProjection {
+                left: -HALF_SIZE,
+                right: HALF_SIZE,
+                bottom: -HALF_SIZE,
+                top: HALF_SIZE,
+                near: -10.0 * HALF_SIZE,
+                far: 10.0 * HALF_SIZE,
+                ..default()
+            },
+            shadows_enabled: true,
             ..default()
         },
-        transform: Transform::from_xyz(10.0, 5.0, 0.0).looking_at(Vec3::ZERO, Vec3::Y),
+        transform: Transform {
+            translation: Vec3::new(0.0, 2.0, 0.0),
+            rotation: Quat::from_rotation_x(-PI / 4.),
+            ..default()
+        },
         ..default()
     });
+
+    // AMBIENT LIGHT
+    commands.insert_resource(AmbientLight {
+        color: Color::WHITE,
+        brightness: 0.15,
+    });
+
+    // FPS TEXT
+    commands.spawn((
+        // Create a TextBundle that has a Text with a list of sections.
+        TextBundle::from_sections([
+            TextSection::new(
+                "FPS: ",
+                TextStyle {
+                    font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                    font_size: 60.0,
+                    color: Color::WHITE,
+                },
+            ),
+            TextSection::from_style(TextStyle {
+                font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                font_size: 60.0,
+                color: Color::GOLD,
+            }),
+        ])
+        .with_style(Style {
+            position_type: PositionType::Absolute,
+            position: UiRect {
+                top: Val::Px(5.0),
+                left: Val::Px(5.0),
+                ..default()
+            },
+            ..Default::default()
+        }),
+        FpsText,
+    ));
+
+    // PARTICLE COUNT TEXT
+    commands.spawn((
+        // Create a TextBundle that has a Text with a list of sections.
+        TextBundle::from_sections([
+            TextSection::new(
+                "PARTICLES: ",
+                TextStyle {
+                    font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                    font_size: 60.0,
+                    color: Color::WHITE,
+                },
+            ),
+            TextSection::from_style(TextStyle {
+                font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                font_size: 60.0,
+                color: Color::GOLD,
+            }),
+        ])
+        .with_style(Style {
+            position_type: PositionType::Absolute,
+            position: UiRect {
+                bottom: Val::Px(5.0),
+                left: Val::Px(5.0),
+                ..default()
+            },
+            ..Default::default()
+        }),
+        NumOfParticlesText,
+    ));
 }
 
 fn camera_controls(
@@ -65,7 +156,7 @@ fn camera_controls(
     left.y = 0.0;
     left = left.normalize();
 
-    let speed = 2.0;
+    let speed = 10.0;
     let rotation_speed = 0.3;
 
     if keyboard_input.pressed(KeyCode::W) {
@@ -98,5 +189,72 @@ fn camera_controls(
 
     if keyboard_input.pressed(KeyCode::X) {
         camera.rotate_axis(Vec3::X, -rotation_speed * time.delta_seconds());
+    }
+}
+
+#[derive(Default, Resource)]
+struct GlobalState {
+    base_velocity: f32,
+    spawn_time: f32,
+    spawn_amount: u32,
+    lifetime: f32,
+    bounds: bool,
+    gravity_strength: f32,
+    example: bool,
+}
+
+fn configure_global_state(mut state: ResMut<GlobalState>) {
+    state.base_velocity = 2.0;
+    state.spawn_time = 0.5;
+    state.spawn_amount = 10;
+    state.lifetime = 5.0;
+    state.bounds = true;
+    state.example = true;
+    state.gravity_strength = 9.81;
+}
+
+fn ui(mut egui_context: ResMut<EguiContext>, mut state: ResMut<GlobalState>) {
+    egui::Window::new("Hello world")
+        .default_size([300.0, 100.0])
+        .show(egui_context.ctx_mut(), |ui| {
+            // Slider
+            ui.add(egui::Slider::new(&mut state.base_velocity, 1.0..=20.0).text("Base velocity"));
+            ui.add(egui::Slider::new(&mut state.lifetime, 0.5..=10.0).text("Lifetime"));
+            ui.add(egui::Slider::new(&mut state.spawn_amount, 1..=50).text("Spawn amount"));
+            ui.add(egui::Slider::new(&mut state.spawn_time, 0.1..=2.0).text("Spawn time"));
+            ui.add(
+                egui::Slider::new(&mut state.gravity_strength, 0.0..=20.0).text("Gravity strength"),
+            );
+
+            // Checkbox
+            ui.add(egui::Checkbox::new(&mut state.bounds, "Bounds"));
+            ui.add(egui::Checkbox::new(&mut state.example, "Example"));
+        });
+}
+
+#[derive(Component)]
+struct FpsText;
+
+#[derive(Component)]
+struct NumOfParticlesText;
+
+fn fps_update_system(diagnostics: Res<Diagnostics>, mut query: Query<&mut Text, With<FpsText>>) {
+    for mut text in &mut query {
+        if let Some(fps) = diagnostics.get(FrameTimeDiagnosticsPlugin::FPS) {
+            if let Some(value) = fps.smoothed() {
+                // Update the value of the second section
+                text.sections[1].value = format!("{value:.2}");
+            }
+        }
+    }
+}
+
+fn particle_count_update_system(
+    mut query: Query<&mut Text, With<NumOfParticlesText>>,
+    query2: Query<&Lifetime>,
+) {
+    for mut text in &mut query {
+        let count = query2.iter().len();
+        text.sections[1].value = format!("{}", count);
     }
 }
--- src/particle.rs
+++ src/particle.rs
@@ -1,5 +1,12 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, utils::HashMap};
 use rand::Rng;
+
+use crate::GlobalState;
+
+const DAMPING: f32 = 0.8;
+const WORLD_SIZE: f32 = 10.0;
+const ALPHA: f32 = 2.0;
+const BETA: f32 = 0.1;
 
 pub struct ParticlePlugin;
 
@@ -7,17 +14,13 @@ pub struct ParticlePlugin;
 #[reflect(Component)]
 pub struct ParticleSpawner {
     spawn_timer: Timer,
-    spawn_amount: usize,
     particle_count: usize,
 }
 
 #[derive(Reflect, Component, Default)]
 #[reflect(Component)]
-pub struct Particle {
+pub struct ParticleVelocity {
     velocity: Vec3,
-    color: Color,
-    size: f32,
-    lifetime: f32,
 }
 
 #[derive(Reflect, Component, Default)]
@@ -26,19 +29,25 @@ pub struct Lifetime {
     timer: Timer,
 }
 
+#[derive(Reflect, Component, Default)]
+#[reflect(Component)]
+pub struct ParticleSize {
+    size: f32,
+}
+
 impl Plugin for ParticlePlugin {
     fn build(&self, app: &mut App) {
         app.add_startup_system(create_particle_spawner)
             .add_system(particle_spawner)
             .add_system(cleanup)
-            .register_type::<Particle>();
+            .add_system(update_particle_location)
+            .register_type::<ParticleVelocity>();
     }
 }
 
 fn create_particle_spawner(mut commands: Commands) {
     commands.spawn({}).insert(ParticleSpawner {
-        spawn_timer: Timer::from_seconds(0.3, TimerMode::Repeating),
-        spawn_amount: 100,
+        spawn_timer: Timer::from_seconds(0.25, TimerMode::Repeating),
         particle_count: 0,
     });
 }
@@ -49,33 +58,54 @@ fn particle_spawner(
     mut materials: ResMut<Assets<StandardMaterial>>,
     mut world: Query<&mut ParticleSpawner>,
     time: Res<Time>,
+    state: Res<GlobalState>,
 ) {
     let mut world = world.single_mut();
     world.spawn_timer.tick(time.delta());
 
     if world.spawn_timer.finished() {
         let mut rng = rand::thread_rng();
-        println!("Particle count: {}", world.particle_count);
+        let mut size;
 
         // Spawn a `spawn_amount` of particles
-        for _ in 0..world.spawn_amount {
+        for _ in 0..state.spawn_amount {
+            size = rng.gen_range(0.1..1.0);
             commands
                 .spawn(PbrBundle {
                     mesh: meshes.add(Mesh::from(shape::Icosphere {
-                        radius: 0.1,
+                        radius: size,
                         subdivisions: 3,
                     })),
-                    material: materials.add(Color::rgb(0.5, 0.5, 0.5).into()),
+                    material: materials.add(StandardMaterial {
+                        base_color: Color::rgba(
+                            rng.gen_range(0.0..1.0),
+                            rng.gen_range(0.0..1.0),
+                            rng.gen_range(0.0..1.0),
+                            0.8,
+                        )
+                        .into(),
+                        // emissive: Color::rgb(1.0, 0.0, 0.0).into(),
+                        alpha_mode: AlphaMode::Blend,
+                        ..Default::default()
+                    }),
                     transform: Transform::from_xyz(
-                        rng.gen_range(-5.0..5.0),
-                        rng.gen_range(-5.0..5.0),
-                        rng.gen_range(-5.0..5.0),
+                        rng.gen_range(-4.0..4.0),
+                        0.0,
+                        rng.gen_range(-4.0..4.0),
                     ),
                     ..Default::default()
                 })
                 .insert(Lifetime {
-                    timer: Timer::from_seconds(1.0, TimerMode::Once),
+                    timer: Timer::from_seconds(state.lifetime, TimerMode::Once),
                 })
+                .insert(ParticleVelocity {
+                    velocity: Vec3::new(
+                        rng.gen_range(-1.0..1.0) * state.base_velocity,
+                        1.0 * state.base_velocity,
+                        rng.gen_range(-1.0..1.0) * state.base_velocity,
+                    ),
+                })
+                .insert(ParticleSize { size: size })
                 .insert(Name::new(format!("Particle {}", world.particle_count)));
             world.particle_count += 1;
         }
@@ -87,6 +117,152 @@ fn cleanup(mut commands: Commands, mut particles: Query<(Entity, &mut Lifetime)>
         lifetime.timer.tick(time.delta());
         if lifetime.timer.finished() {
             commands.entity(entity).despawn_recursive();
+        }
+    }
+}
+
+fn update_particle_location(
+    mut particles: Query<(Entity, &ParticleSize, &mut ParticleVelocity, &mut Transform)>,
+    time: Res<Time>,
+    state: Res<GlobalState>,
+) {
+    let mut map = HashMap::new();
+    if state.example {
+        for (entity, size, velocity, transform) in &particles {
+            for (entity_2, size_2, velocity_2, transform_2) in &particles {
+                if (transform.translation - transform_2.translation)
+                    .dot(velocity.velocity - velocity_2.velocity)
+                    < 0.0
+                    && transform.translation.distance(transform_2.translation)
+                        < (size.size + size_2.size)
+                {
+                    let n1 = (transform_2.translation - transform.translation)
+                        / transform_2.translation.distance(transform.translation);
+                    let n2 = (transform.translation - transform_2.translation)
+                        / transform.translation.distance(transform_2.translation);
+
+                    let v1 = velocity.velocity - (1.0 + DAMPING) * n2.dot(velocity.velocity) * n2;
+                    let v2 =
+                        velocity_2.velocity - (1.0 + DAMPING) * n1.dot(velocity_2.velocity) * n1;
+
+                    if !map.contains_key(&entity.index()) {
+                        map.insert(entity.index(), v1);
+                    }
+
+                    if !map.contains_key(&entity_2.index()) {
+                        map.insert(entity_2.index(), v2);
+                    }
+                }
+            }
+
+            // Collision with box
+            if state.bounds {
+                let v;
+                if !map.contains_key(&entity.index()) {
+                    map.insert(entity.index(), velocity.velocity);
+                }
+                v = map.get_mut(&entity.index()).unwrap();
+
+                if transform.translation.x < -WORLD_SIZE {
+                    *v -= (1.0 + DAMPING)
+                        * (v.dot(Vec3::new(1.0, 0.0, 0.0)))
+                        * Vec3::new(1.0, 0.0, 0.0);
+                }
+
+                if transform.translation.x > WORLD_SIZE {
+                    *v -= (1.0 + DAMPING)
+                        * (v.dot(Vec3::new(-1.0, 0.0, 0.0)))
+                        * Vec3::new(-1.0, 0.0, 0.0);
+                }
+
+                if transform.translation.z < -WORLD_SIZE {
+                    *v -= (1.0 + DAMPING)
+                        * (v.dot(Vec3::new(0.0, 0.0, 1.0)))
+                        * Vec3::new(0.0, 0.0, 1.0);
+                }
+
+                if transform.translation.z > WORLD_SIZE {
+                    *v -= (1.0 + DAMPING)
+                        * (v.dot(Vec3::new(0.0, 0.0, -1.0)))
+                        * Vec3::new(0.0, 0.0, -1.0);
+                }
+
+                if transform.translation.y < -WORLD_SIZE {
+                    *v -= (1.0 + DAMPING)
+                        * (v.dot(Vec3::new(0.0, 1.0, 0.0)))
+                        * Vec3::new(0.0, 1.0, 0.0);
+                }
+
+                if transform.translation.y > WORLD_SIZE {
+                    *v -= (1.0 + DAMPING)
+                        * (v.dot(Vec3::new(0.0, -1.0, 0.0)))
+                        * Vec3::new(0.0, -1.0, 0.0);
+                }
+            }
+        }
+
+        for (entity, _, mut velocity, mut transform) in &mut particles {
+            if map.contains_key(&entity.index()) {
+                velocity.velocity = *map.get(&entity.index()).unwrap();
+            }
+            // Apply gravity
+            velocity.velocity.y -= state.gravity_strength * time.delta_seconds();
+            transform.translation += velocity.velocity * time.delta_seconds();
+        }
+    } else {
+        // Direction
+        let mut d;
+        for (entity, _, velocity, transform) in &particles {
+            d = Vec3::new(0.0, 0.0, 0.0);
+            for (_, _, _, transform_2) in &particles {
+                d += (transform.translation - transform_2.translation)
+                    / (transform_2.translation.distance(transform.translation) + 1.0).powf(ALPHA);
+            }
+
+            if state.bounds {
+                // Distance to planes of sorounding box
+                if (transform.translation.x - WORLD_SIZE).abs() < 2.0 {
+                    d.x = -(transform.translation.x - WORLD_SIZE).abs()
+                        / ((transform.translation.x - WORLD_SIZE).abs() + 1.0).powf(ALPHA);
+                }
+
+                if (transform.translation.x + WORLD_SIZE).abs() < 2.0 {
+                    d.x = (transform.translation.x + WORLD_SIZE).abs()
+                        / ((transform.translation.x + WORLD_SIZE).abs() + 1.0).powf(ALPHA);
+                }
+
+                if (transform.translation.z - WORLD_SIZE).abs() < 2.0 {
+                    d.z = -(transform.translation.z - WORLD_SIZE).abs()
+                        / ((transform.translation.z - WORLD_SIZE).abs() + 1.0).powf(ALPHA);
+                }
+
+                if (transform.translation.z + WORLD_SIZE).abs() < 2.0 {
+                    d.z = (transform.translation.z + WORLD_SIZE).abs()
+                        / ((transform.translation.z + WORLD_SIZE).abs() + 1.0).powf(ALPHA);
+                }
+
+                if (transform.translation.y - WORLD_SIZE).abs() < 2.0 {
+                    d.y = -(transform.translation.y - WORLD_SIZE).abs()
+                        / ((transform.translation.y - WORLD_SIZE).abs() + 1.0).powf(ALPHA);
+                }
+
+                if (transform.translation.y + WORLD_SIZE).abs() < 2.0 {
+                    d.y = (transform.translation.y + WORLD_SIZE).abs()
+                        / ((transform.translation.y + WORLD_SIZE).abs() + 1.0).powf(ALPHA);
+                }
+            }
+
+            map.insert(entity.index(), (1.0 - BETA) * velocity.velocity + BETA * d);
+        }
+
+        for (entity, _, mut velocity, mut transform) in &mut particles {
+            if map.contains_key(&entity.index()) {
+                velocity.velocity = *map.get(&entity.index()).unwrap();
+            }
+
+            // Apply gravity
+            velocity.velocity.y -= state.gravity_strength * time.delta_seconds();
+            transform.translation += velocity.velocity * time.delta_seconds();
         }
     }
 }
